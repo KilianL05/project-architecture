30fde17205464d1afd97c35c15da84dedafd85b3
Jules Artaud <jules.artaud@outlook.com>
2025-04-30 14:48:34 +0200

    routes api setup

--- Bill/dependencies.js
+++ Bill/dependencies.js
@@ -3,6 +3,7 @@ const config = require('./config');
 
 function getStorageService() {
   return new MinIOStorageService(
+    console.log('Initializing MinIO Storage Service'),
     config.MINIO_ENDPOINT,
     config.MINIO_ACCESS_KEY,
     config.MINIO_SECRET_KEY
--- Bill/index.js
+++ Bill/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const storageRouter = require('./routes/storageRouter');
+const storageRouter = require('./routes/api/storageRouter');
+const apiRouter = require('./routes/apiRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/storage', storageRouter);
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
